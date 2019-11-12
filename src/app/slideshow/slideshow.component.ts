6f527f3a2ced8d6150ee1e4968bfe9b2fffa7e21
GiacomoTosin <giacomo.tosin@marconirovereto.it>
2019-11-12 14:48:18 +0100

    5 files modified

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -13,15 +13,23 @@ export class SlideshowComponent implements OnInit {
   }
   x = 0;
 
-  link = ['',
-  '',
-  '',
+  link = ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQNGfD_Vi3uXEc8cjUcUPOseRKf0xwAKhe6ZSyJMKj0BKQTBox&s',
+
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKmpED4bZ1hav6blg_MarzwQZa9uNSHSjH_Qc55kg1TZD3wLrU&s',
+
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBc9OnaAFgzkN0VmMSag_duouM4k_suo_8Rzubf6o3xeQHTiv1&s',
+
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRgsN9VxiqLKJBCOFvdJzH59vRTfoa1lPk9wKGFsMQzO4o6D0j&s',
+
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnSgbeoHhSqjWSQo3CNdtGvP0-1GyPTAhR9scBlwbwPpEJXu5h&s',
+  
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDaeFz6yILI7GlL8jzqqWy99WRXj3EWM9pY4Q8gf9g7S9Fv1NuNg&s'
   ];
 
   numero = this.link[this.x];
 
   destra(){
-    if(this.x ==10){
+    if(this.x == 5){
       this.x = 0;
       this.numero = this.link[this.x];
     }else{
@@ -31,7 +39,7 @@ export class SlideshowComponent implements OnInit {
   }
   sinistra(){
         if(this.x ==0){
-      this.x = 10;
+      this.x = 5;
       this.numero = this.link[this.x];
     }else{
       this.x = this.x - 1;
